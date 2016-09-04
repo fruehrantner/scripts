84fb31e28dee09ff656493ed375acdf611dba627
fruehrantner <fruehrantner@outlook.com>
2016-09-04 22:16:32 +0200

    fixed Base64 encoding of UTF-8 files

--- data-encoding/encode-lines-to-base64.pl
+++ data-encoding/encode-lines-to-base64.pl
@@ -6,11 +6,12 @@ use warnings;
 
 use File::Slurper qw(read_lines);
 use MIME::Base64 qw(encode_base64);
+use Encode;
 
 
 my @file = read_lines("/etc/hosts");
 
 foreach (@file) {
-	print encode_base64($_,"");
+	print encode_base64((Encode::encode_utf8($_)),"");
 	print "\n";
 }
