585ad9e732e0624f732046fa6a371ff1a460db26
fruehrantner <fruehrantner@outlook.com>
2016-10-04 23:19:06 +0200

    added script to print Unicode characters

---
+++ data-generation/print-unicode-characters.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use 5.016;
+use strict;
+use warnings;
+
+
+binmode(STDOUT, ":utf8");
+
+print "\N{WHITE SMILING FACE}";
+print "\N{WARNING SIGN}";
+print "\N{RADIOACTIVE SIGN}";
+print "\N{BIOHAZARD SIGN}";
+print "\N{SKULL AND CROSSBONES}";
+print "\N{U+2603}";
+print chr(0x2713);
+print "\x{2682}";
+print "\n";
